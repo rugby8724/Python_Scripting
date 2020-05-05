57fc9b9b058d2546006720a4e2318bda210bbb85
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2020-05-05 15:57:19 -0500

    Twitter Bot

--- twitter_bot/tweet.py
+++ twitter_bot/tweet.py
@@ -1,11 +1,35 @@
 import tweepy
-import secret
+import time
+from nolooking import secret
 
-auth = tweepy.OAuthHandler('twitter_api_key', 'twitter_api_secret')
-auth.set_access_token('twitter_key', 'twitter_secret')
+auth = tweepy.OAuthHandler(secret.twitter_api_key, secret.twitter_api_secret)
+auth.set_access_token(secret.twitter_key, secret.twitter_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(2000)
+
+
+search_string = 'Jeremiah Wise'
+numberOfTweets = 5
+
+for tweet in tweepy.Cursor(api.search, search_string).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('I liked that tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+
+
+#Genrous Bot
+# for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+#
+#     print(follower.name)
